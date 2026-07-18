ad07222632f779960d72ac9006746af43db3aa0b
Kunal <kunal3052.beai24@chitkara.edu.in>
2026-07-19 03:58:46 +0530

    Substring finder implementation in c

---
+++ C_basics/substringSearch.c
@@ -0,0 +1,26 @@
+int find_substring(const char* haystack, const char* needle) {
+  int h_len = 0, n_len = 0;
+
+  while (haystack[h_len] != '\0') h_len++;
+  while (needle[n_len] != '\0') n_len++;
+
+  for (int i = 0; i < h_len; i++) {
+    char sub[100];
+    int index = 0;
+    for (int j = i; j < h_len && index < n_len; j++) {
+      sub[index++] = haystack[j];
+    }
+    sub[index] = '\0';
+    int match = 1;
+    for (int k = 0; k < n_len; k++) {
+      if (sub[k] != needle[k]) {
+        match = 0;
+        break;
+      }
+    }
+    if (match) {
+      return i;
+    }
+  }
+  return -1;
+}
