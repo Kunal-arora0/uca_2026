0485f53b4452c6cf3744aab810feb0b7448f8545
Kunal <kunal3052.beai24@chitkara.edu.in>
2026-07-19 04:02:43 +0530

    Substring Search Implementation in c

--- C_basics/substringSearch.c
+++ C_basics/substringSearch.c
@@ -1,26 +1,35 @@
-int find_substring(const char* haystack, const char* needle) {
-  int h_len = 0, n_len = 0;
+#include <stdio.h>
 
-  while (haystack[h_len] != '\0') h_len++;
-  while (needle[n_len] != '\0') n_len++;
+int find_substring(const char *haystack, const char *needle) {
+    int h_len = 0, n_len = 0;
+    while (haystack[h_len] != '\0') h_len++;
+    while (needle[n_len] != '\0') n_len++;
+    for (int i = 0; i < h_len; i++) {
+        char sub[100];
+        int index = 0;
+        for (int j = i; j < h_len && index < n_len; j++) {
+            sub[index++] = haystack[j];
+        }
+        sub[index] = '\0';
+        int match = 1;
+        for (int k = 0; k < n_len; k++) {
+            if (sub[k] != needle[k]) {
+                match = 0;
+                break;
+            }
+        }
+        if (match) {
+            return i;
+        }
+    }
+    return -1;
+}
 
-  for (int i = 0; i < h_len; i++) {
-    char sub[100];
-    int index = 0;
-    for (int j = i; j < h_len && index < n_len; j++) {
-      sub[index++] = haystack[j];
-    }
-    sub[index] = '\0';
-    int match = 1;
-    for (int k = 0; k < n_len; k++) {
-      if (sub[k] != needle[k]) {
-        match = 0;
-        break;
-      }
-    }
-    if (match) {
-      return i;
-    }
-  }
-  return -1;
+int main() {
+    char *haystack = "Embedded Systems";
+    char *needle = "bed";
+
+    printf("%d\n", find_substring(haystack, needle));
+
+    return 0;
 }
